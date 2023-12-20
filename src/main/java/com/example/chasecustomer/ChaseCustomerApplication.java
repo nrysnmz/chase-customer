2036823802018b4bd3689a7ecea6c8caa0994a6e
Nuray Korganci <nrysnmz@gmail.com>
2023-12-20 10:46:30 -0600

    m

--- src/main/java/com/example/chasecustomer/ChaseCustomerApplication.java
+++ src/main/java/com/example/chasecustomer/ChaseCustomerApplication.java
@@ -3,8 +3,10 @@ package com.example.chasecustomer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy(proxyTargetClass = true)
 public class ChaseCustomerApplication {
 
 	public static void main(String[] args) {
